9abd209e9ee1c9215d3cb9cb12966fb2db37ae4e
Karol <karol.m.kostyra@gmail.com>
2020-06-15 13:37:31 +0200

    Testing of moving a paddle by mouse

---
+++ Blocknado/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float screenWidthUnits = 16f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float mousePos = (Input.mousePosition.x / Screen.width * screenWidthUnits); //in units
+        Vector2 paddlePos = new Vector2(mousePos, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
