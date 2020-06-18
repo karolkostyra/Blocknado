afb78c87f3a96ed9f7966adca891209a14a10417
Karol <karol.m.kostyra@gmail.com>
2020-06-18 23:00:19 +0200

    Test of detection a ball direction
    - set execution order for ball and paddle scripts (forgot click 'apply' in previous commit)
    - testing of a new function which is using to detection a ball direction (when is sticked to paddle)

--- Blocknado/Assets/Scripts/Ball.cs
+++ Blocknado/Assets/Scripts/Ball.cs
@@ -7,16 +7,19 @@ public class Ball : MonoBehaviour
     [SerializeField] private Paddle paddle;
 
     private Vector2 paddleBallVector; // distance between center of paddle and ball
+    private Vector3 lastBallPos;
 
     private void Start()
     {
         SetStartingPos();
+        UpdateLastBallPos();
         paddleBallVector = transform.position - paddle.transform.position;
     }
 
     private void Update()
     {
-        StickBallToPaddle();
+        StickToPaddle();
+        DetectMouseDirection();
     }
 
     private void SetStartingPos()
@@ -24,9 +27,33 @@ public class Ball : MonoBehaviour
         transform.position = new Vector2(paddle.transform.position.x, paddle.transform.position.y + 0.45f);
     }
 
-    private void StickBallToPaddle()
+    private void StickToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePos + paddleBallVector;
+    }
+
+    private void DetectMouseDirection()
+    {
+        if(lastBallPos.x > transform.position.x)
+        {
+            Debug.Log("left");
+            UpdateLastBallPos();
+        }
+        else if(lastBallPos.x < transform.position.x)
+        {
+            Debug.Log("right");
+            UpdateLastBallPos();
+        }
+        else
+        {
+            Debug.Log("up");
+            UpdateLastBallPos();
+        }
+    }
+
+    private void UpdateLastBallPos()
+    {
+        lastBallPos = transform.position;
     }
 }
