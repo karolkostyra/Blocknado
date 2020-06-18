eb001c29448120c6d0864a33a57f39393a3a0229
Karol <karol.m.kostyra@gmail.com>
2020-06-18 23:29:23 +0200

    Ball launching
    - implemented a new function to launching ball
    - used method to detection the ball direction to sideways launching
    - removed friction from 2D physic material linked to the ball

--- Blocknado/Assets/Scripts/Ball.cs
+++ Blocknado/Assets/Scripts/Ball.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] private Paddle paddle;
+    [SerializeField] private float xPush;
+    [SerializeField] private float yPush;
 
     private Vector2 paddleBallVector; // distance between center of paddle and ball
     private Vector3 lastBallPos;
+    private bool isLaunched;
+
+    private Vector3 mouseDelta = Vector3.zero;
+    private Vector3 lastMousePosition = Vector3.zero;
+
 
     private void Start()
     {
+        isLaunched = false;
         SetStartingPos();
         UpdateLastBallPos();
         paddleBallVector = transform.position - paddle.transform.position;
@@ -18,8 +26,16 @@ public class Ball : MonoBehaviour
 
     private void Update()
     {
-        StickToPaddle();
+        mouseDelta = Input.mousePosition - lastMousePosition;
+        lastMousePosition = Input.mousePosition;
+        //Debug.Log("SPEED: " + mouseDelta);
+
         DetectMouseDirection();
+        if (!isLaunched)
+        {
+            StickToPaddle();
+            LaunchOnMouseClick();
+        }
     }
 
     private void SetStartingPos()
@@ -37,17 +53,20 @@ public class Ball : MonoBehaviour
     {
         if(lastBallPos.x > transform.position.x)
         {
-            Debug.Log("left");
+            //Debug.Log("left");
+            xPush = 1f * (mouseDelta.x/5f);
             UpdateLastBallPos();
         }
         else if(lastBallPos.x < transform.position.x)
         {
-            Debug.Log("right");
+            //Debug.Log("right");
+            xPush = 1f * (mouseDelta.x / 5f);
             UpdateLastBallPos();
         }
         else
         {
-            Debug.Log("up");
+            //Debug.Log("up");
+            xPush = 0f;
             UpdateLastBallPos();
         }
     }
@@ -55,5 +74,14 @@ public class Ball : MonoBehaviour
     private void UpdateLastBallPos()
     {
         lastBallPos = transform.position;
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isLaunched = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
     }
 }
