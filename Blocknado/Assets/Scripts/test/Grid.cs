b20136b74eaaca0544f99f0586cdf5387deb9754
Karol <karol.m.kostyra@gmail.com>
2020-06-19 20:11:53 +0200

    Destroying blocks and grid on game space
    - changed a block (sprite) size to unity standard size 1 = 1 unit
    - created a simple Block class with method to destroys the blocks after collision with a ball
    - implemented a new script to generate grid on game space which will be used in the future to instantiate blocks to creating levels

---
+++ Blocknado/Assets/Scripts/test/Grid.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    private float size = 1f;
+
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        position -= transform.position;
+
+        int xCount = Mathf.RoundToInt(position.x / size);
+        int yCount = Mathf.RoundToInt(position.y / size);
+        int zCount = 0;
+
+        Vector3 result = new Vector3(
+            (float)xCount * size,
+            (float)yCount * size,
+            (float)zCount);
+
+        result += transform.position;
+
+        return result;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (float x = 0; x < 16; x += size)
+        {
+            for (float y = 0; y < 12; y += size)
+            {
+                var point = GetNearestPointOnGrid(new Vector3(x, y, 0f));
+                Gizmos.DrawSphere(point+new Vector3(0.5f,0.5f,0), 0.1f);
+            }
+
+        }
+    }
+}
