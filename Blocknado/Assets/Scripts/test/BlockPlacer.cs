5003a331e1629c45cb2f0f17f48dcaced281f18f
Karol <karol.m.kostyra@gmail.com>
2020-06-20 22:23:15 +0200

    Creating blocks on a grid
    - added new script to instantiate blocks on a grid (from previous commit) in game mode

---
+++ Blocknado/Assets/Scripts/test/BlockPlacer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BlockPlacer : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer block;
+    private Grid grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            PlaceBlockNear(ray);
+            
+        }
+    }
+
+    private void PlaceBlockNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        //GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+        GameObject.Instantiate(block).transform.position = finalPosition;
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+    }
+}
