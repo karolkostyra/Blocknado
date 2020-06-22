0d6cec514a88e8ec842dd92748de93b246d50a6d
Karol <karol.m.kostyra@gmail.com>
2020-06-22 14:24:15 +0200

    Editor method to instantiate blocks with set up positions from loaded file
    - creating prefabs as a childs
    - refactoring of editor window script
    - working function to saving data about blocks (positions x and y) into json
    - implemented a working function to create blocks with positions which are data loaded from a file (in editor mode)

--- Blocknado/Assets/Scripts/Editor/Blocknado.cs
+++ Blocknado/Assets/Scripts/Editor/Blocknado.cs
@@ -21,19 +21,15 @@ public class Blocknado : EditorWindow
 
         GUILayout.Label("Own functions in editor:", EditorStyles.boldLabel);
 
-        if (GUILayout.Button("Select solid and clone it as GOAL", editorButton, GUILayout.Height(30)))
+        if (GUILayout.Button("Instantiate + set positions from loaded data", editorButton, GUILayout.Height(30)))
         {
             InstantiateBlocks();
-        }
-        if (GUILayout.Button("Instantiate", editorButton, GUILayout.Height(30)))
-        {
-            InstantiateBlocks2();
         }
         if (GUILayout.Button("SaveArray", editorButton, GUILayout.Height(30)))
         {
             Save();
         }
-        if (GUILayout.Button("LoadArray", editorButton, GUILayout.Height(30)))
+        if (GUILayout.Button("Load and debug.log", editorButton, GUILayout.Height(30)))
         {
             Load();
         }
@@ -41,26 +37,22 @@ public class Blocknado : EditorWindow
 
     private void InstantiateBlocks()
     {
-        blockPlacer = FindObjectOfType<BlockPlacer>();
-        for (int i = 0; i < blockPlacer.list.Count; i++)
-        {
-            Debug.Log("test?");
-            PrefabUtility.InstantiatePrefab(blockPlacer.list[i]);
-        }
-    }
-
-    private void InstantiateBlocks2()
-    {
         string path = "Assets/Prefabs";
-        List<string> prefabNames = new List<string>() { "Block" };
+        string prefabName = "Block";
+        GameObject parent = GameObject.FindGameObjectWithTag("Blocks");
 
-        for (int i = 0; i < prefabNames.Count; i++)
+        blockPlacer = FindObjectOfType<BlockPlacer>();
+        var list = blockPlacer.LoadArray();
+
+        for (int i = 0; i < list.Count; i=i+2)
         {
-            Object prefab = AssetDatabase.LoadAssetAtPath(path + "/" + prefabNames[i]
+            Object prefab = AssetDatabase.LoadAssetAtPath(path + "/" + prefabName
                 + ".prefab", typeof(GameObject));
             if (prefab != null)
             {
-                PrefabUtility.InstantiatePrefab(prefab);
+                GameObject obj = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                obj.transform.SetParent(parent.transform);
+                obj.transform.position = new Vector2(list[i], list[i + 1]);
             }
         }
     }
@@ -76,6 +68,11 @@ public class Blocknado : EditorWindow
     {
         blockPlacer = FindObjectOfType<BlockPlacer>();
 
-        blockPlacer.LoadArray();
+        var list = blockPlacer.LoadArray();
+
+        for (int i = 0; i < list.Count; i=i+2)
+        {
+            Debug.Log("x: " + list[i] + ", y: " + list[i + 1]);
+        }
     }
 }
--- Blocknado/Assets/Scripts/LevelDesign/BlockPlacer.cs
+++ Blocknado/Assets/Scripts/LevelDesign/BlockPlacer.cs
@@ -7,15 +7,14 @@ using System.Runtime.Serialization.Formatters.Binary;
 public class BlockPlacer : MonoBehaviour
 {
     [SerializeField] private GameObject block;
+    [SerializeField] private BlocksData _BlockData = new BlocksData();
     private Grid grid;
     public List<GameObject> list = new List<GameObject>();
-
-    [SerializeField] private BlocksData _BlockData = new BlocksData();
 
     [System.Serializable]
     public class BlocksData
     {
-        public string blockName;
+        public List<float> blocksList = new List<float>();
     }
 
 
@@ -24,43 +23,32 @@ public class BlockPlacer : MonoBehaviour
         grid = FindObjectOfType<Grid>();
     }
 
-    private void Start()
-    {
-        if(list == null)
-        {
-            list = new List<GameObject>(0);
-        }
-    }
-
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             PlaceBlockNear(ray);
-            //SaveArray();
         }
     }
 
     private void PlaceBlockNear(Vector3 clickPoint)
     {
         var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
-        //GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
         GameObject obj = Instantiate(block);
         obj.transform.position = finalPosition;
         list.Add(obj);
-        //PrefabUtility.InstantiatePrefab(obj);
-        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
     }
 
     public void SaveArray()
     {
+        SaveDataBlocks();
         string block = JsonUtility.ToJson(_BlockData);
         Debug.Log(block);
         System.IO.File.WriteAllText(Application.persistentDataPath + "/BlocksData.json", block);
     }
 
-    public void LoadArray()
+    public List<float> LoadArray()
     {
         string filePath = System.IO.Path.Combine(Application.persistentDataPath, "BlocksData.json");
         
@@ -69,12 +57,23 @@ public class BlockPlacer : MonoBehaviour
         {
             string data = System.IO.File.ReadAllText(filePath);
             BlocksData blocksData = JsonUtility.FromJson<BlocksData>(data);
-            Debug.Log(blocksData.blockName);
+            //Debug.Log(blocksData.blocksList);
+            return blocksData.blocksList;
+            
         }
         else
         {
             Debug.Log("ERROR - FILE NOT FOUND!");
-            return;
+            return null;
+        }
+    }
+
+    private void SaveDataBlocks()
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            _BlockData.blocksList.Add(list[i].transform.position.x);
+            _BlockData.blocksList.Add(list[i].transform.position.y);
         }
     }
 }
